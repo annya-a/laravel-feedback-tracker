5eef365234145bd7710302fe6bcd7cbc631a3549
annya <ali.annya@gmail.com>
2019-01-04 12:58:58 +0200

    Change navigation

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,11 +1,39 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <title>Feature Requests</title>
-    <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Clean Blog - Start Bootstrap Theme</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 </head>
+
 <body>
+
+<!-- Navigation -->
+@include('layouts.partials.navigation')
+
+<!-- Main Content -->
+<div class="container">
     @yield('layouts.master.content')
+</div>
+
+<hr>
+
+<!-- Footer -->
+@include('layouts.partials.footer')
+
+<!-- Bootstrap core JavaScript -->
+<script src="{{ mix('/js/app.js') }}"></script>
+
 </body>
+
 </html>
