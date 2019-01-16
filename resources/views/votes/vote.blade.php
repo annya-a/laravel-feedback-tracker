f31bf7e3cc908993b7b623db3321481c764bae48
annya <ali.annya@gmail.com>
2019-01-16 17:23:02 +0200

    Don't allow user to vote for own post

--- resources/views/votes/vote.blade.php
+++ resources/views/votes/vote.blade.php
@@ -2,8 +2,8 @@
     <form method="POST" action="{{ route('posts.vote', $post) }}">
         @csrf
 
-        <button class="btn btn-slight btn-middle" data-loading-text=" ... ">
-            <i class="fas fa-caret-up"></i>
+        <button class="btn @can ('vote', $post) btn-slight @endcan btn-middle btn-vote" @cannot('vote', $post) disabled @endcannot>
+            <i class="fas @can('vote', $post) fa-caret-up @endcan"></i>
             <div class="text-dark">{{ $post->votes_count }}</div>
         </button>
     </form>
