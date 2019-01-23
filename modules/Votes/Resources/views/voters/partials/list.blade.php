c165d9cebc60dc3ba851ffd2b2573e583953201e
annya <ali.annya@gmail.com>
2019-01-23 12:20:01 +0200

    Add empty text for voters list

--- modules/Votes/Resources/views/voters/partials/list.blade.php
+++ modules/Votes/Resources/views/voters/partials/list.blade.php
@@ -1,10 +1,15 @@
-@foreach($voters as $voter)
-    <div class="voter-item">
-        <div class="float-left mr-2">
-            <img class="rounded-circle avatar-sm" src="{{ asset($voter->getFirstMediaUrl('avatar', 'thumb')) }}" />
+@if ($voters->isNotEmpty())
+    @foreach($voters as $voter)
+        <div class="voter-item">
+            <div class="float-left mr-2">
+                <img class="rounded-circle avatar-sm" src="{{ asset($voter->getFirstMediaUrl('avatar', 'thumb')) }}" />
+            </div>
+            <div>
+                {{ $voter->name }}
+            </div>
         </div>
-        <div>
-            {{ $voter->name }}
-        </div>
-    </div>
-@endforeach
+    @endforeach
+@else
+    {{ __('No voters yet. Be the first who votes this post!') }}
+@endif
+
